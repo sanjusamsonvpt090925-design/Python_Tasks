b3813a8c7bb5a9e1e8faa1d86d84d5b08e1ee4fb
sanjusamsonvpt090925-design <sanjusamsonvpt090925@gmail.com>
2025-10-02 12:58:27 +0530

    Add files via upload

---
+++ Assignment 2/Task1.py
@@ -0,0 +1,13 @@
+#program to find the given number is even or odd using if else statement
+
+#geting input from use
+
+n=int(input("Enter a number: "))
+      
+#if condition
+      
+if (n%2==0):
+    print("The given number is Even!\n")
+else:
+    print("The given number is Odd!\n")
+print("\t\t\tThankyou! Have a nice Day...")
---
+++ Assignment 2/Task2.py
@@ -0,0 +1,16 @@
+#program to print the sum of number b/w 1 to 50 using for loop
+
+
+
+# Initialize sum variable
+sum = 0
+
+# Loop from 1 to 50 
+for i in range(1, 51):
+    sum += i
+
+# Print the result
+print("The sum of numbers from 1 to 50 is: ", sum)
+print('\n')
+print('\t\t\tThankyou! Have a nice day..')
+
